9b5da9b5258393fbaf59dce323cf8dab1bd1a863
Gil Fernandes <gil.fernandes@onepointltd.com>
2024-06-27 11:35:37 +0100

    Formatting.

--- src/components/ExtraFunctionButtons.tsx
+++ src/components/ExtraFunctionButtons.tsx
@@ -1,11 +1,11 @@
-import {Message} from "../model/message.ts";
-import {FaHourglassHalf} from "react-icons/fa";
-import {useContext, useEffect, useRef} from "react";
-import {AppContext} from "../context/AppContext.tsx";
-import {ChatContext} from "../context/ChatContext.tsx";
+import { Message } from "../model/message.ts";
+import { FaHourglassHalf } from "react-icons/fa";
+import { useContext, useEffect, useRef } from "react";
+import { AppContext } from "../context/AppContext.tsx";
+import { ChatContext } from "../context/ChatContext.tsx";
 import MarkdownComponent from "./Markdown.tsx";
-import {WEBSOCKET_SERVER_COMMAND} from "../model/websocketCommands.ts";
-import {GrContract, GrExpand} from "react-icons/gr";
+import { WEBSOCKET_SERVER_COMMAND } from "../model/websocketCommands.ts";
+import { GrContract, GrExpand } from "react-icons/gr";
 
 /**
  * Used to display the question clarification.
@@ -19,15 +19,15 @@ export default function ExtraFunctionButtons() {
     updatingExpectedNodes,
     clarificationClicked,
     showClarification,
-    setShowClarification
+    setShowClarification,
   } = useContext(AppContext);
-  const {socket} = useContext(ChatContext);
+  const { socket } = useContext(ChatContext);
   const clarificationRef = useRef<HTMLDivElement>(null);
 
   const message: Message = messages[currentMessage];
 
   useEffect(() => {
-    if(clarificationClicked) {
+    if (clarificationClicked) {
       setShowClarification(true);
     }
   }, [clarificationClicked]);
@@ -58,25 +58,25 @@ export default function ExtraFunctionButtons() {
     }
   }
 
-
   if (currentMessage === 0 || message.final_report) return null;
 
   console.log("showClarification", showClarification);
 
   return (
     <div className="clarification">
-      {updatingExpectedNodes && <FaHourglassHalf/>}
+      {updatingExpectedNodes && <FaHourglassHalf />}
       {message.clarification && (
-        <section ref={clarificationRef}
-                 className={`clarification-main relative ${showClarification ? "expanded" : "contracted"}`}
+        <section
+          ref={clarificationRef}
+          className={`clarification-main relative ${showClarification ? "expanded" : "contracted"}`}
         >
-          <MarkdownComponent content={message.clarification}/>
+          <MarkdownComponent content={message.clarification} />
           <div className="clarification-contract">
             {showClarification && (
-              <GrContract onClick={() => setShowClarification(false)}/>
+              <GrContract onClick={() => setShowClarification(false)} />
             )}
             {!showClarification && (
-              <GrExpand onClick={() => setShowClarification(true)}/>
+              <GrExpand onClick={() => setShowClarification(true)} />
             )}
           </div>
         </section>
