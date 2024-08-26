2ebbecdbacd42d20f5fd910f9bc1161671644e26
Gil Fernandes <gil.fernandes@onepointltd.com>
2024-08-26 14:32:03 +0000

    Added server shell script

--- server.sh
+++ server.sh
@@ -1,3 +1,9 @@
+#! /bin/bash
+source /etc/profile
+echo Executed profile
+export PATH=/home/ubuntu/.nvm/versions/node/v18.19.1/bin:/home/ubuntu/miniconda3/bin:/home/ubuntu/miniconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
+cd /home/ubuntu/projects/data-wellness-companion-ui
 yarn
 yarn run build
 npx http-server ./dist --port 8088 -P http://127.0.0.1:8088? -o .
