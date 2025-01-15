bbb0bbcde40ab9a181465c5dfa611611dab690de
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-01-15 17:36:45 +0000

    Reformatted.

--- src/components/menu/TermsButton.tsx
+++ src/components/menu/TermsButton.tsx
@@ -9,7 +9,12 @@ export default function TermsButton() {
   return (
     <MenuItemTemplate
       title="Terms"
-      func={() => window.open("https://www.onepointltd.com/wp-content/uploads/2025/01/Terms-of-Use-for-Onepoint-D-Well-v2.0.pdf", "_blank")}
+      func={() =>
+        window.open(
+          "https://www.onepointltd.com/wp-content/uploads/2025/01/Terms-of-Use-for-Onepoint-D-Well-v2.0.pdf",
+          "_blank",
+        )
+      }
     >
       <InfoImage />
     </MenuItemTemplate>
