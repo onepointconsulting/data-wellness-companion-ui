b54759d5a96d0c46b5132d9bc01b93876595ab92
Blessy <blessy.athisayamani@onepointltd.com>
2025-04-25 22:45:15 +0530

    Updated facicon, navigation icon and loader gif

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -11,7 +11,7 @@ export default function Spinner({ size = 24 }: { size?: number }) {
     <SpinnerLayout>
       <span className="text-sm text-gray-500 mt-3 -mb-4 mx-auto">
         <img
-          src="./D-Well_Icons_one_frame.gif"
+          src="./ResAI_animation.gif"
           alt={t("Please wait")}
           title={t("Please wait")}
           className={`h-${size} w-${size}`}
