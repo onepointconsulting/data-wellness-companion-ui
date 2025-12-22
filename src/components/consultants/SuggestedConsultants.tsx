b5f3fbf9fcd9cf8c9ed02524063cdc3368a23a2d
Gil Fernandes <gil.fernandes@onepointltd.com>
2025-12-22 17:44:40 +0000

    Moved images for specific release to dedicated folder

--- src/components/consultants/SuggestedConsultants.tsx
+++ src/components/consultants/SuggestedConsultants.tsx
@@ -26,7 +26,7 @@ function ConsultantCard({
           src={
             rating.linkedin_photo_400 ??
             rating.linkedin_photo_200 ??
-            "res_ai_logo_square.avif"
+            "res-ai/res_ai_logo_square.avif"
           }
           className="w-full grayscale hover:grayscale-0 transition duration-300 ease-in-out"
         />
