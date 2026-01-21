cf9d52d76a877a43219dbca02756222e40f98ab3
Gil Fernandes <gil.fernandes@onepointltd.com>
2026-01-21 13:10:56 +0000

    Fixed build errors

--- src/components/dialogue/IntroDialogue.tsx
+++ src/components/dialogue/IntroDialogue.tsx
@@ -77,6 +77,7 @@ export default function IntroDialogue() {
                 suggestions: [],
                 clarification: "",
                 question_id: undefined,
+                documents: [],
               }}
               handleSuggestion={() => {}}
             />
