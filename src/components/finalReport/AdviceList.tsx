8f046af1ced00f41676fdc95913f15b81302f38d
Gil Fernandes <gil.fernandes@onepointltd.com>
2026-01-22 14:32:28 +0000

    Fixed color of text of deep research whilst it is processing

--- src/components/finalReport/AdviceList.tsx
+++ src/components/finalReport/AdviceList.tsx
@@ -45,7 +45,7 @@ function StatusUpdateMessage({
 }) {
   return (
     <div className="flex flex-row justify-end mt-0">
-      <span key={statusKey} className="text-sm subtle-blink">
+      <span key={statusKey} className="text-sm subtle-blink dark:text-gray-100">
         {message}
       </span>
     </div>
